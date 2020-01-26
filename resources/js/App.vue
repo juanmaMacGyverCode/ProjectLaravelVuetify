d1d2d3ac390ef413c434f77948a716ffa0beeb3a
juanma <jmlpdr.93@gmail.com>
2020-01-26 13:04:33 +0000

    ProjectInitialized

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <v-alert :value="true" type="success">
+            You have succesfully installed Vuetify 2
+        </v-alert>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/*Vue.component('example-component', require('./components/ExampleComponent.vue').default);*/
+
+import vuetify from './vuetify'
+
+import App from "./App.vue";
+const app = new Vue({
+    vuetify,
+    render: h => h(App), 
+    el: '#app',
+});
